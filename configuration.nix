dff404b496e8b4f9e90185557ea2365ffcbde5e9
System administrator <root@ip-172-31-13-103.ap-south-1.compute.internal>
2024-08-13 21:29:59 +0000

    :sparkles: FIRST commit

---
+++ configuration.nix
@@ -0,0 +1,17 @@
+{ modulesPath,pkgs, ... }: {
+  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+  ec2.efi = true;
+  system.stateVersion = "24.05";  # Set the appropriate version
+  
+   environment.systemPackages = with pkgs; [
+    vim # adding vim editor
+
+    htop # adding htop for monitoring
+    neofetch
+    bat
+    cmatrix
+
+   ]
+;
+  
+}
